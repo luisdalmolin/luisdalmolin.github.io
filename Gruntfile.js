12846978c80a80810f2970feda0e6c8ad0375deb
Luis Dalmolin <luis.nh@gmail.com>
2014-07-22 13:06:40 -0300

    Atualizado poertfolio - Configurado SASS

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,11 +18,9 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-notify');
 
-    grunt.registerTask('default', ['jshint', 'uglify', 'compass', 'watch']);
+    grunt.registerTask('default', ['compass', 'watch']);
 };
